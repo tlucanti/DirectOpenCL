bfa1ba52e51cfae76612fc72230cb6fde9e0625f
tlucanti <prototype3628800@yandex.ru>
2024-04-05 23:32:28 +0300

    stream: client: add second socket
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

--- py/frontend/util/WebFrontend.py
+++ py/frontend/util/WebFrontend.py
@@ -2,8 +2,9 @@
 import numpy as np
 import pickle
 import time
+import threading
 
-from .NetSock import Client
+from .NetSock import TcpClient
 
 def time_report(names, times):
     assert len(names) + 1 == len(times)
@@ -11,77 +12,76 @@ def time_report(names, times):
     total = times[-1] - times[0]
     for i in range(len(names)):
         delta = times[i + 1] - times[i]
-        print(f'{names[i]}:\t{round(delta * 1000)}ms\t({delta / total * 100:.1f}%)')
+        print(f'{names[i]}:\t{round(delta * 1000)}ms\t({delta / total * 100:.1f}%)\tpossible fps: {float("inf") if delta == 0 else 1.0 / delta:.1f}')
     print(f'total:\t {round(total * 1000)}ms (fps: {1 / total:.1f})')
     print()
 
 
 class WebFrontend():
     def __init__(self, constructor, host, port=7777):
-        self.__client = Client(host, port)
+        self.__event_socket = TcpClient(host, port)
+        pix_socket = TcpClient(host, port + 1)
 
-        event = self.__client.recv_string(1)
+        event = self.__event_socket.recv_char()
         assert event == 'R'
-        width = self.__client.recv_number()
-        height = self.__client.recv_number()
+        self.__width = self.__event_socket.recv_number()
+        self.__height = self.__event_socket.recv_number()
 
-        self.__frontend = constructor(width, height)
+        self.__frontend = constructor(self.__width, self.__height)
         self.__frontend.key_hook(self.__key_callback)
 
+        threading.Thread(target=self.__draw_thread, args=[pix_socket]).start()
+
         while True:
-            start = time.time()
-            event = self.__client.recv_char()
-            if event == 'P':
-                size = self.__client.recv_number()
-                # print(f'client: getting pickle of size {size}')
-                prep = time.time()
-
-                data = self.__client.do_recv(size)
-                recv = time.time()
-
-                # print(f'client: got pickle of size {len(data)}')
-                data = pickle.loads(data)
-                decode = time.time()
-
-                # print(f'client: got array {data}')
-                self.__frontend.draw(data)
-                draw = time.time()
-
-                time_report(['prep', 'recv', 'decode', 'draw'], [start, prep, recv, decode, draw])
-
-            elif event == 'B':
-                compression = self.__client.recv_char()
-
-                if compression   == '0':
-                    size = self.__client.recv_number()
-                    prep = time.time()
-
-                    data = self.__client.do_recv(size)
-                    recv = time.time()
-
-                    width, height = self.__frontend.width(), self.__frontend.height()
-                    recv = time.time()
-
-                    assert len(data) == size
-                    assert size == width * height * 3
-
-                    array = np.frombuffer(data, dtype=np.uint8)
-                    array = array.reshape((height, width, 3))
-                    decode = time.time()
-
-                    self.__frontend.draw(array)
-                    draw = time.time()
-
-                time_report(['prep', 'recv', 'decode', 'draw'], [start, prep, recv, decode, draw])
-            elif event == 'm':
+            event = self.__event_socket.recv_char()
+            if event == 'm':
                 x, y = self.__frontend.mouse()
-                self.__client.send_string('M')
-                self.__client.send_number(x)
-                self.__client.send_number(y)
+                self.__event_socket.send_string('M')
+                self.__event_socket.send_number(x)
+                self.__event_socket.send_number(y)
             else:
                 print(f'client: unknown event {event}')
 
+    def __draw_thread(self, pix_socket):
+        encoding = pix_socket.recv_char()
+        if encoding == 'B':
+            encoding += pix_socket.recv_char()
+
+        if encoding == 'P':
+            size = pix_socket.recv_number()
+            while True:
+                start = time.time()
+
+                data = pix_socket.do_recv(size)
+                assert len(data) == size
+                recv = time.time()
+
+                data = pickle.loads(data)
+                decode = time.time()
+
+                self.__frontend.draw(data)
+                draw = time.time()
+
+                time_report(['recv', 'decode', 'draw'], [start, recv, decode, draw])
+
+        elif encoding == 'B0':
+            size = pix_socket.recv_number()
+            while True:
+                start = time.time()
+
+                data = pix_socket.do_recv(size)
+                assert len(data) == size
+                recv = time.time()
+
+                array = np.frombuffer(data, dtype=np.uint8)
+                array = array.reshape((self.__height, self.__width, 3))
+                decode = time.time()
+
+                self.__frontend.draw(array)
+                draw = time.time()
+
+                time_report(['recv', 'decode', 'draw'], [start, recv, decode, draw])
 
     def __key_callback(self, winid, keycode, pressed):
-        self.__client.send_string('K' if pressed else 'k')
-        self.__client.send_number(keycode)
+        self.__event_socket.send_string('K' if pressed else 'k')
+        self.__event_socket.send_number(keycode)
