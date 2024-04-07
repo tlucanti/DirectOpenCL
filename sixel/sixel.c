62e1528cccd9eacd48d6ac5686fac65ff575ea22
tlucanti <prototype3628800@yandex.ru>
2024-04-07 13:45:38 +0000

    sixel: update interface
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

--- sixel/sixel.c
+++ sixel/sixel.c
@@ -14,43 +14,52 @@
 #include <unistd.h>
 #include <errno.h>
 
-#ifndef CONFIG_SIXEL_RAW_MODE
-# define CONFIG_SIXEL_RAW_MODE true
-#endif
-
-#ifndef CONFIG_KEYBOARD_ENABLE
-# define CONFIG_KEYBOARD_ENABLE true
-#endif
-
-#define DEBUG
-
 #define ESCAPE_CLEAR_SCREEN "\033[0;0H"
 #define ESCAPE_HIDE_CURSOR "\033[?25l"
 #define ESCAPE_SHOW_CURSOR "\033[?25h"
 #define ESCAPE_ENABLE_LOCATOR "\033[?1003h" "\033[?1015h" "\033[?1006h" "\033[?1016h"
 #define ESCAPE_DISABLE_LOCATOR "\e[?1000l"
 
-//#define WANR_UNKNOWN_ESCAPE() fprintf(stderr, "unknow escape sequence\n")
-#define WANR_UNKNOWN_ESCAPE() /* empty */
+#if CONFIG_GUILIB_SIXEL_WARN_UNKWONN_ESCAPE
+# define WANR_UNKNOWN_ESCAPE() fprintf(stderr, "unknow escape sequence\n")
+#else
+# define WANR_UNKNOWN_ESCAPE() /* empty */
+#endif
 
-#define DELTA 0.1f
-#define MAX_FPS_RELAX 1000
+#define MAX_FPS CONFIG_GUILIB_SIXEL_MAX_FPS_RELAX
+#define DELAY CONFIG_GUILIB_SIXEL_KEYBOARD_DELAY
 
-static struct termios g_term_info;
-static volatile int g_mouse_x = 0;
-static volatile int g_mouse_y = 0;
-static volatile key_hook_t g_callback = NULL;
-static struct gui_window *g_window = NULL;
-static volatile bool wfi_flag;
-static pthread_spinlock_t stdout_lock;
-static pthread_spinlock_t key_lock;
-static volatile bool key_reader_thread_should_stop = false;
+struct gui_window {
+	unsigned int width;
+	unsigned int height;
+	unsigned int length;
+	key_hook_t callback;
+	int mouse_x;
+	int mouse_y;
+	unsigned int *raw_pixels;
+	sixel_output_t *sixel_output;
+	sixel_dither_t *sixel_dither;
+};
 
 struct key_metadata {
 	double last_press_time;
 	int keycode;
 	bool released;
 };
+
+static struct termios g_term_info;
+
+static volatile int g_mouse_x = 0;
+static volatile int g_mouse_y = 0;
+static volatile key_hook_t g_callback = NULL;
+
+static struct gui_window *g_window = NULL;
+
+static volatile bool wfi_flag;
+static volatile bool key_reader_thread_should_stop = false;
+
+static pthread_spinlock_t stdout_lock;
+static pthread_spinlock_t key_lock;
 
 static void fps_relax(void)
 {
@@ -64,8 +73,8 @@ static void fps_relax(void)
 	do {
 		clock_gettime(CLOCK_MONOTONIC, &ts);
 		fps = 1.f / ((ts.tv_sec - prev_s) + (ts.tv_nsec - prev_ns) * 1e-9f);
-		//usleep(5000);
-	} while (fps > MAX_FPS_RELAX);
+		usleep(5000);
+	} while (fps > MAX_FPS);
 }
 
 static void set_wfi_flag(void)
@@ -87,7 +96,9 @@ static void key_callback(int keycode, bool pressed)
 
 static void string_write(int fd, const char *s)
 {
-	write(fd, s, strlen(s));
+	if (write(fd, s, strlen(s)) < 0) {
+                gui_panic("string write error");
+        }
 }
 
 static void locator_enable(bool enable)
@@ -102,7 +113,7 @@ static void locator_enable(bool enable)
 static int tty_init(void)
 {
 	if (tcgetattr(STDIN_FILENO, &g_term_info) != 0) {
-		perror("tty_init() failed");
+		gui_perror("tcsetattr fail");
 		return EINVAL;
 	}
 	return 0;
@@ -114,7 +125,7 @@ static int tty_raw(bool enable)
 
 	if (!enable) {
 		if (tcsetattr(STDIN_FILENO, TCSANOW, &g_term_info) != 0) {
-			perror("tty restore failed: ");
+			gui_perror("tcsetattr fail");
 			return EINVAL;
 		}
 		return 0;
@@ -124,7 +135,7 @@ static int tty_raw(bool enable)
 	cfmakeraw(&raw_mode);
 
 	if (tcsetattr(STDIN_FILENO, TCSANOW, &raw_mode) != 0) {
-		perror("tty set raw failed: ");
+		gui_perror("tcsetattr fail");
 		tty_raw(false);
 		return EINVAL;
 	}
@@ -135,7 +146,9 @@ static int tty_raw(bool enable)
 static int sixel_write(char *data, int size, void *arg)
 {
 	(void)arg;
-	write(STDOUT_FILENO, data, size);
+	if (write(STDOUT_FILENO, data, size) < 0) {
+                gui_panic("stdout write fail");
+        }
 	return 0;
 }
 
@@ -151,9 +164,9 @@ static void *release_checker_thread(void *arg)
 {
 	struct key_metadata *meta = arg;
 
-	usleep(DELTA * 1e6f);
+	usleep(DELAY * 1e6f);
 	pthread_spin_lock(&key_lock);
-	if (time_now() - meta->last_press_time > DELTA && !meta->released) {
+	if (time_now() - meta->last_press_time > DELAY && !meta->released) {
 		meta->released = true;
 		pthread_spin_unlock(&key_lock);
 		g_callback(g_window, meta->keycode, false);
@@ -187,20 +200,24 @@ static void key_reader_event_tracker(int keycode)
 
 	if (keycode >= 256) {
 		pthread_spin_lock(&stdout_lock);
-		fprintf(stderr, "\nkey overflow\n");
+                gui_perror("keycode overflow");
 		pthread_spin_unlock(&stdout_lock);
-		abort();
+                return;
 	}
 
 	time = time_now();
-	do_callback = (time - meta[keycode].last_press_time > DELTA);
+	do_callback = (time - meta[keycode].last_press_time > DELAY);
 	meta[keycode].last_press_time = time;
 	if (pthread_create(&tid, NULL, release_checker_thread, &meta[keycode])) {
-		perror("release_checker_thread spawn fail");
+		pthread_spin_lock(&stdout_lock);
+		gui_perror("release_checker_thread spawn fail");
+		pthread_spin_unlock(&stdout_lock);
 		abort();
 	}
 	if (pthread_detach(tid)) {
-		perror("release_checker_thread detach fail");
+		pthread_spin_lock(&stdout_lock);
+		gui_perror("release_checker_thread detach fail");
+		pthread_spin_unlock(&stdout_lock);
 		abort();
 	}
 
@@ -228,7 +245,7 @@ static void *key_reader_thread(void *arg)
 			goto read_fail;
 		}
 
-		if (!CONFIG_SIXEL_RAW_MODE && c == '\n') {
+		if (!CONFIG_GUILIB_SIXEL_RAW_MODE && c == '\n') {
 			continue;
 		}
 
@@ -313,6 +330,7 @@ static void *key_reader_thread(void *arg)
 			}
 		} else if (c == 'x') {
 			pthread_spin_lock(&stdout_lock);
+                        fprintf(stderr, "perssed 'x' exiting now\n");
 			gui_finalize();
 			pthread_spin_unlock(&stdout_lock);
 			set_wfi_flag();
@@ -326,37 +344,37 @@ static void *key_reader_thread(void *arg)
 
 read_fail:
 	pthread_spin_lock(&stdout_lock);
-	perror("key_reader_thread: stdin read error");
-	pthread_spin_unlock(&stdout_lock);
-	abort();
+	gui_panic("stdin read error");
 }
 
-void gui_bootstrap(void)
+int gui_bootstrap(void)
 {
 	pthread_t tid;
 
 	if (pthread_spin_init(&stdout_lock, false)) {
-		perror("stdout_lock init fail");
+                gui_perror("stdout_lock init fail");
 		goto fail;
 	}
 	if (pthread_spin_init(&key_lock, false)) {
-		perror("key_lock init fail");
+		gui_perror("key_lock init fail");
 		goto fail;
 	}
 
-	if (CONFIG_KEYBOARD_ENABLE) {
+	if (CONFIG_GUILIB_SIXEL_KEYBOARD_ENABLE) {
 		if (pthread_create(&tid, NULL, key_reader_thread, NULL)) {
-			perror("key_reader_thread spawn fail");
+			gui_perror("key_reader_thread spawn fail");
 			goto fail;
 		}
 		locator_enable(true);
 	}
 
-	if (CONFIG_SIXEL_RAW_MODE) {
+	if (CONFIG_GUILIB_SIXEL_RAW_MODE) {
 		if (tty_init()) {
+                        gui_perror("tty init fail");
 			goto fail;
 		}
 		if (tty_raw(true)) {
+                        gui_perror("tty enable raw mode fail");
 			goto fail;
 		}
 	}
@@ -365,130 +383,156 @@ void gui_bootstrap(void)
 	string_write(STDOUT_FILENO, ESCAPE_HIDE_CURSOR);
 	pthread_spin_unlock(&stdout_lock);
 
-	return;
+	return 0;
+
 fail:
 	key_reader_thread_should_stop = true;
 	pthread_spin_destroy(&key_lock);
 	pthread_spin_destroy(&stdout_lock);
-	abort();
+	return EFAULT;
 }
 
-void gui_finalize(void)
+int gui_finalize(void)
 {
-	if (CONFIG_SIXEL_RAW_MODE) {
+	if (CONFIG_GUILIB_SIXEL_RAW_MODE) {
 		if (tty_raw(false)) {
-			abort();
+                        gui_perror("tty disable raw mode fail");
+                        return EFAULT;
 		}
 	}
+
 	locator_enable(false);
 	string_write(STDOUT_FILENO, ESCAPE_SHOW_CURSOR);
+        return 0;
 }
 
-void gui_create(struct gui_window *window, unsigned int width, unsigned int height)
+struct gui_window *gui_alloc(void)
+{
+        return malloc(sizeof(struct gui_window));
+}
+
+int gui_create(struct gui_window *window, unsigned int width, unsigned int height)
 {
 	SIXELSTATUS status;
+        int err;
 
-	window->__width = width;
-	window->__height = height;
-	window->__length = width * height;
-	window->__raw_pixels = malloc(sizeof(unsigned int) * window->__length);
 	g_window = window;
 
-	if (window->__raw_pixels == NULL) {
-		perror("window buffer alloc fail");
+	window->width = width;
+	window->height = height;
+	window->length = width * height;
+        window->raw_pixels = NULL;
+        window->sixel_output = NULL;
+        window->sixel_dither = NULL;
+
+	window->raw_pixels = malloc(sizeof(unsigned int) * window->length);
+	if (window->raw_pixels == NULL) {
+		gui_perror("out of memory");
+                err = ENOMEM;
 		goto fail;
 	}
 
-	status = sixel_output_new(&window->__output, sixel_write, stdout, NULL);
+	status = sixel_output_new(&window->sixel_output, sixel_write, stdout, NULL);
 	if (SIXEL_FAILED(status)) {
-		perror("sixel_output_new() fail");
+		gui_perror("create sixel output fail");
+                err = EFAULT;
 		goto fail;
 	}
 
-	status = sixel_dither_new(&window->__dither, SIXEL_PALETTE_MAX, NULL);
+	status = sixel_dither_new(&window->sixel_dither, SIXEL_PALETTE_MAX, NULL);
 	if (SIXEL_FAILED(status)) {
-		perror("sixel_dither_new() fail");
+		gui_perror("sixel sixel dither fail");
+                err = EFAULT;
 		goto fail;
 	}
 
-	sixel_output_set_encode_policy(window->__output, SIXEL_ENCODEPOLICY_FAST);
-	return;
+	sixel_output_set_encode_policy(window->sixel_output, SIXEL_ENCODEPOLICY_FAST);
+	return 0;
+
 fail:
-	gui_finalize();
-	abort();
+        gui_destroy(window);
+        return err;
 }
 
-void gui_destroy(struct gui_window *window)
+int gui_destroy(struct gui_window *window)
 {
-	free(window->__raw_pixels);
+	free(window->raw_pixels);
+        sixel_output_destroy(window->sixel_output);
+        sixel_dither_destroy(window->sixel_dither);
+        return 0;
 }
 
-unsigned int gui_width(const struct gui_window *window)
+unsigned int gui_width(struct gui_window *window)
 {
-	return window->__width;
+	return window->width;
 }
 
-unsigned int gui_height(const struct gui_window *window)
+unsigned int gui_height(struct gui_window *window)
 {
-	return window->__height;
+	return window->height;
 }
 
 void gui_set_pixel(struct gui_window *window, unsigned x, unsigned y, unsigned color)
 {
-#ifdef DEBUG
+#ifdef CONFIG_GUILIB_DEBUG
 	if (x >= gui_width(window) || y >= gui_height(window)) {
 		fprintf(stderr, "\r\ngui_set_pixel out of bounds: %u %u\n", x, y);
 	}
 #endif
-	gui_set_pixel_raw(window, (unsigned long)y * window->__width + x, color);
+	gui_set_pixel_raw(window, (unsigned long)y * window->width + x, color);
 }
 
 int gui_set_pixel_safe(struct gui_window *window, unsigned x, unsigned y, unsigned color)
 {
-	unsigned long i = (unsigned long)y * window->__width + x;
+	unsigned long i = (unsigned long)y * window->width + x;
 
-	if (i < window->__length) {
+	if (i < window->length) {
 		gui_set_pixel_raw(window, i, color);
 		return 0;
 	} else {
-		return 1;
+		return EINVAL;
 	}
 }
 
 void gui_set_pixel_raw(struct gui_window *window, unsigned long i, unsigned color)
 {
-	window->__raw_pixels[i] = color;
+	window->raw_pixels[i] = color;
 }
 
-unsigned *gui_raw_pixels(const struct gui_window *window)
+unsigned *gui_raw_pixels(struct gui_window *window)
 {
-	return window->__raw_pixels;
+	return window->raw_pixels;
 }
 
-void gui_draw(const struct gui_window *window)
+int gui_draw(struct gui_window *window)
 {
 	SIXELSTATUS status;
 
 	fps_relax();
 
-	sixel_dither_initialize(window->__dither, (void *)window->__raw_pixels,
-				window->__width, window->__height,
-				SIXEL_PIXELFORMAT_BGRA8888, SIXEL_LARGE_AUTO,
-				SIXEL_REP_AUTO, SIXEL_QUALITY_FULL);
+	status = sixel_dither_initialize(
+		window->sixel_dither, (void *)window->raw_pixels, window->width,
+		window->height, SIXEL_PIXELFORMAT_BGRA8888, SIXEL_LARGE_AUTO,
+		SIXEL_REP_AUTO, SIXEL_QUALITY_FULL);
+        if (SIXEL_FAILED(status)) {
+                gui_perror("sixel dither initialize fail");
+                return EFAULT;
+        }
 
 	pthread_spin_lock(&stdout_lock);
 	string_write(STDOUT_FILENO, ESCAPE_CLEAR_SCREEN);
-	status = sixel_encode((void *)window->__raw_pixels, window->__width,
-			      window->__height, 0, window->__dither,
-			      window->__output);
+	status = sixel_encode((void *)window->raw_pixels, window->width,
+			      window->height, 0, window->sixel_dither,
+			      window->sixel_output);
+	if (SIXEL_FAILED(status)) {
+                gui_perror("sixel encode fail");
+	        pthread_spin_unlock(&stdout_lock);
+                return EFAULT;
+	}
 	string_write(STDOUT_FILENO, "\r\n");
 	pthread_spin_unlock(&stdout_lock);
 
-	if (SIXEL_FAILED(status)) {
-		fprintf(stderr, "sixel_encode() fail\n");
-		gui_finalize();
-		abort();
-	}
+        return 0;
 }
 
 void gui_key_hook(struct gui_window *window, key_hook_t hook)
@@ -497,7 +541,7 @@ void gui_key_hook(struct gui_window *window, key_hook_t hook)
 	g_callback = hook;
 }
 
-void gui_mouse(const struct gui_window *window, int *x, int *y)
+void gui_mouse(struct gui_window *window, int *x, int *y)
 {
 	(void)window;
 	*x = g_mouse_x;
