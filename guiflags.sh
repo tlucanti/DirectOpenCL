8109c558fc0d2975295ba207db0afb35fdd4dc58
tlucanti <prototype3628800@yandex.ru>
2024-05-19 09:02:11 +0300

    core: add selection of root directory to guiflags.sh
    Signed-off-by: tlucanti <prototype3628800@yandex.ru>

--- guiflags.sh
+++ guiflags.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# give path to the root of GUIlib directory to $1 argument, otherwise it will
+# default root directory to "../GUIlib" relative path
+
+if [[ $1 == '' ]]; then
+	export GUIPATH="../GUIlib"
+else
+	export GUIPATH=$1
+fi
+
 if [[ $GUILIB == '' ]]; then
         echo '============================== [GUILIB ERROR] ==============================' >&2
         echo 'env $GUILIB should be set to "opengl", "sixel" or "stream"' >&2
@@ -9,14 +18,14 @@ if [[ $GUILIB == '' ]]; then
 fi
 
 if [[ $GUILIB == 'sixel' ]]; then
-        echo -n ' -L ../GUIlib -L ../GUIlib/thirdparty/sixel -lgui-sixel -lsixel '
+        echo -n " -L $GUIPATH -L $GUIPATH/thirdparty/sixel -lgui-sixel -lsixel "
 elif [[ $GUILIB == 'stream' ]]; then
-        echo -n ' -L ../GUIlib -lgui-stream -ljpeg '
+        echo -n " -L $GUIPATH -lgui-stream -ljpeg "
 elif [[ $GUILIB == 'opengl' ]]; then
-	echo -n ' -L ../GUIlib -lgui-opengl -lOpenGL -lglfw '
+	echo -n " -L $GUIPATH -lgui-opengl -lOpenGL -lglfw "
 else
         echo '============================== [GUILIB ERROR] ==============================' >&2
-        echo 'invalid GUILIB type: ' $GUILIB ', expected "sixel" or "stream" '>&2
+        echo 'invalid GUILIB type: ' $GUILIB ', expected "opengl", "sixel" or "stream" '>&2
         echo '============================================================================' >&2
         exit 1
 fi
